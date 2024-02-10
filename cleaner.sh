d0402d026fe871c8b20196315491e38090e436fa
root <root@2280629-hz51881.twc1.net>
2024-02-10 13:22:37 +0300

    cleaner

--- cleaner.sh
+++ cleaner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-DIR="/var/log/test"
+DIR="/var/log"
 find $DIR -name "*.log.*" -exec echo "Удаляю-" {} \;
 find $DIR -name "*.log.*" -exec rm {} \;
 echo "Очистка хлама завершена!"
